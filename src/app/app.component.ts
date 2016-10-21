22daf1c7369f3882cfd044313652735c0bceb03f
Felix Isensee <f.isensee@gmail.com>
2016-10-21 18:26:32 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  page = 'home';
+
+  states = {
+    'home': true,
+    'projects': false,
+    'blog': false
+  };
+
+  setAllStatesFalls(): void {
+    this.states.home = false;
+    this.states.projects = false;
+    this.states.blog = false;
+  }
+
+  setActiveIcon(page: string): void {
+    this.setAllStatesFalls();
+    this.states[page] = true;
+  }
+
+  changeContent(panel: string): void {
+    this.page = panel;
+    this.setActiveIcon(panel);
+  }
 }
